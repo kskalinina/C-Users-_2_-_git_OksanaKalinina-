0404be122ed0a4511f9a9216b1aab91487c840c7
kskalinina <oksana.os@list.ru>
2023-04-08 17:53:16 +0300

    JS 12

--- main.js
+++ main.js
@@ -1,39 +1,52 @@
-const appenBlock = (block) => document.getElementById('app').appendChild(block);
-const randomRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const appendBlock =(block) => document.getElementById('app').appendChild(block);
+const randomRange =(min, max) => Math.floor(Math.random() * (max - min) + min)
 const generateColor = () => {
-  
+  const isNotEmpty = Math.random() > 0.5;
+  if (isNotEmpty) {
+    return "red";
+  }
+  return null;
 }
-const createBlock = (x, y, size = 50, color) => {
-  const result = document.createElement('div');
+const createBlock =(x , y , size, color) => {
+  const result = document.createElement('div')
   if (color) {
     result.style.backgroundColor = color;
-  } else {
-    const isNotEmpty = Math.random() > 0.5;
   }
-  if (isNotEmpty) {
-    result.style.backgroundColor = color;
-  }
-  result.style.width = result.style.height = `${size}px`;
-  result.style.position = 'absolute';
-  result.style.left = '${x}px';
-  result.style.top = '${y}px';
+  result.style.width = result.style.height = `${size}px`
+  result.style.position = "absolute";
+  result.style.left = `${x}px`;
+  result.style.top = `${y}px`;
   return result;
 };
 const BLOCK_SIZE = 50;
-let columns = 5;
-let xPos = 0;
-let yPos = 0;
-const line = [];
+const DIMENSION = 5;
+
+let columns = DIMENSION;
+let rows = columns * 2 ;
+let xPos = 0 ;
+let yPos = 0 ;
 
 
-while (columns-- > 0) {
-  const block = createBlock(xPos, yPos, BLOCK_SIZE);
-  line.push(block);
-  xPos += BLOCK_SIZE;
-  appenBlock(block);
+while (rows-- > 0) {
+  let line = [];
+  xPos = 0;
+columns = DIMENSION ;
+  while (columns-- > 0) {
+    const color = generateColor()
+    const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+    line.push(block);
+    xPos += BLOCK_SIZE;
+    appendBlock(block)
+  }
+
+  line.reverse().forEach((color) => {
+    const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+    appendBlock(block)
+    xPos += BLOCK_SIZE;
+  })
+  yPos += BLOCK_SIZE;
 }
-line.reverse().forEach((item) => {
-  const
-  xPos += BLOCK_SIZE;
-});
+
+
+
 
