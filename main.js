9a3702910d03f5fb54721492a99fbdd069b4480b
kskalinina <oksana.os@list.ru>
2023-04-10 17:43:07 +0300

    button close

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 import "uno.css";
 import "@unocss/reset/tailwind.css";
+
+const dombtnCreatetask = document.getElementById("btnCreatetask");
+const dompopupCreateTask = document.getElementById("popupCreateTask");
+
+const OnPopupCloseButtonClick = () => {
+    dompopupCreateTask.classList.add("hidden");
+};
+dombtnCreatetask.onclick = () => {
+           console.log("> dompopupCreateTask.classList",dompopupCreateTask)
+        dompopupCreateTask.classList.remove("hidden");
+
+           const dombtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup")
+        dombtnCloseCreateTaskPopup.onclick = OnPopupCloseButtonClick ;
+    };
+// dompopupCreateTask.classList.add("hidden")
+// dombtnCloseCreateTaskPopup.onclick = null
+//}
