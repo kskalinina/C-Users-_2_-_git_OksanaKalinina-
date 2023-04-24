c5aa74b392eab450f624140850142e1bff52bb4d
kskalinina <oksana.os@list.ru>
2023-04-24 21:27:36 +0300

    test

--- cypress/e2e/taski.cy.js
+++ cypress/e2e/taski.cy.js
@@ -2,7 +2,7 @@ import Dom from "../../src/constants/dom.js";
 describe('Create Todo', () =>{
     it('user open main page and create task', () => {
         cy.visit('http://localhost:5173/');
-        
+
         cy.get(`#${Dom.Button.CREATE_TASK}`).
         should('exist')
             .should('contain.text', '+Create Task')
