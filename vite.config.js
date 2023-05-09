5c69ca19480b7db37019f9ed5b4ebe7688b6ebfb
kskalinina <kalinina.os@list.ru>
2023-05-09 17:50:42 +0300

    vue intro

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,7 @@
-// vite.config.ts
-import UnoCSS from "unocss/vite";
-import presetUno from "@unocss/preset-uno";
-import presetIcons from '@unocss/preset-icons';
-import presetWebFonts from '@unocss/preset-web-fonts'
-export default {
-    plugins: [
-        UnoCSS( {
-            include: ['./index.html', 'main.js', './src/**/**.js'],
-            presets:[
-                presetUno(),
-                presetIcons({
-                    /* options */
-                }),
-                presetWebFonts({
-                    provider: 'google', // default provider
-                    fonts: {
-                        // these will extend the default theme
-                        sans: 'Noto Savs:400,700',
-                        mono: ['Fira Code', 'Fira Mono:400,700'],
-                        // custom ones
-                        lobster: 'Lobster',
-                        lato: [
-                            {
-                                name: 'Noto Sans',
-                                weights: ['400', '700'],
-                                italic: false,
-                            },
-                            {
-                                name: 'sans-serif',
-                                provider: 'none',
-                            },
-                        ],
-                    },
-                }),
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-            ],
-            rules: [
-                // your custom rules
-            ],
-        }),
-    ],
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
