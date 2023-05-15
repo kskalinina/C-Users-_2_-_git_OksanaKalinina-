3b4b73a90bf5c57617591bad6f1839f8fd39092f
kskalinina <kalinina.os@list.ru>
2023-05-15 10:16:02 +0300

    basic MyName component with events and passing status in props

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,50 @@ import MyName from "./components/MyName.vue";
 </script>
 
 <template>
-  <MyName />
-</template>
+  <select
+    name="status"
+    @change="onSelectChange"
+  >
+    v-if ="!status"
+    <option
+      value="unselected"
+      selected
+    >
+      Unselected
+    </option>
+    <option
+      v-for="item in ['Ready', 'Start', 'Stop']"
+      :key="item"
+      :value="item"
+      :selected="item === status"
+    >
+      {{ item }}
+    </option>
+  </select>
 
+  <MyName
+    :status=" status"
+    @reset="onStatusReset"
+  />
+</template>
+<script>
+export default {
+  data: () => ({
+    status: null,
+}),
+methods:{
+  onSelectChange (event) {
+    console.log("App -> onSelectChange:", event.target.value);
+    this.status = event.target.value;
+  },
+    onStatusReset(){
+      console.log("App -> onStatusReset");
+      this.status = null;
+    }
+  }
+
+};
+</script>
 <style scoped>
 
 </style>
--- src/components/MyName.vue
+++ src/components/MyName.vue
@@ -5,17 +5,62 @@
     <span style="color:red;">
       {{ lastname }}
     </span>
-  </div>;
-</template>
+    <div>
+      <small><span
+               class="flex flex-col"
+               :class="{
+                 'selected': !!status,
+                 'unselected': !status
+               }"
+             >
+               Status:
+             </span>
+
+        <span> {{ status }}</span>
+        <div>
+          <button
+            :style="{
+              visibility: status
+                ? 'visible': 'hidden',
+            }"
+            @click="onResetClick"
+          >Reset</button>
+        </div>
+      </small>
+    </div>
+  </div>
+</template>;
 
 <script>
 export default {
   name: "MyName",
+  props: {
+    status: {
+      type: String,
+      default: "-"
+    },
+  },
+emits: { "reset": null },
   data: () => ({
     firstname: "Oksana",
     lastname: "Kalinina"
-  })
+  }),
+    methods: {
+      onResetClick() {
+        console.log("> My Name -> onResetClick");
+        this.$emit ("reset");
+      }
+    }
+    
+  
+
 };
 </script>
 <style scoped>
+.selected {
+  color: green;
+}
+.unselected {
+  color: red;
+}
 </style>
