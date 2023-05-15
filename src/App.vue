af137f0d37c51564c233629ddb612d171857ef13
kskalinina <kalinina.os@list.ru>
2023-05-15 11:01:50 +0300

    to do list

--- src/App.vue
+++ src/App.vue
@@ -4,48 +4,43 @@ import MyName from "./components/MyName.vue";
 </script>
 
 <template>
-  <select
-    name="status"
-    @change="onSelectChange"
+  <h3> Todo List</h3>
+  <input
+    v-model="inputText"
+    @keyup.enter="canAddItemToTheList && onInputEnterKeyUp()"
   >
-    v-if ="!status"
-    <option
-      value="unselected"
-      selected
-    >
-      Unselected
-    </option>
-    <option
-      v-for="item in ['Ready', 'Start', 'Stop']"
-      :key="item"
-      :value="item"
-      :selected="item === status"
-    >
-      {{ item }}
-    </option>
-  </select>
 
-  <MyName
-    :status=" status"
-    @reset="onStatusReset"
-  />
+  <div>
+    List:
+    <div
+      v-for="(item,index) in todos"
+      :key="item"
+    >
+      {{ index +1 }}) {{ item }}
+    </div>
+  </div>
 </template>
 <script>
 export default {
   data: () => ({
-    status: null,
+    inputText: null,
+    todos: []
 }),
-methods:{
-  onSelectChange (event) {
-    console.log("App -> onSelectChange:", event.target.value);
-    this.status = event.target.value;
-  },
-    onStatusReset(){
-      console.log("App -> onStatusReset");
-      this.status = null;
+  computed: {
+    canAddItemToTheList() {
+      return this.todoText?.length > 0;
+    },
+    todoText() {
+      return this.inputText?.trim();
     }
-  }
-
+  },
+methods: {
+  onInputEnterKeyUp() {
+    console.log("->App-onInputEnterKeyUp",this.todoText);
+    this.todos.push(this.todoText);
+    this.inputText = " ";
+  },
+},
 };
 </script>
 <style scoped>
