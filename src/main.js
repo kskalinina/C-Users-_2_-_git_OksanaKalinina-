f7312037abc385a8068ab6385aa86042272e91c4
oksana kalinina <kalinina.os@list.ru>
2023-06-06 11:12:44 +0300

    pocket base working version Books pagination

--- src/main.js
+++ src/main.js
@@ -10,6 +10,25 @@ import PROVIDE  from "@/constans/PROVICE_PB.js";
 const pb = new PocketBase(import.meta.env.VITE_SERVER_PATH);
 console.log("pb.authStore.isValid:", pb.authStore.isValid);
 
+let db = new PouchDB(`${import.meta.env.VITE_SERVER_DB_PATH}/books`);
+db.get("book1").then((doc)=> {
+  console.log("doc",doc);
+});
+db.changes({
+  since: "now",
+  live: true,
+  include_docs: true
+}).on("change", function(change) {
+  // handle change
+  console.log("change", change);
+}).on("complete", function(info) {
+  // changes() was canceled
+  console.log("complete", info);
+}).on("error", function (err) {
+  console.log(err);
+});
+
+
 createApp(AppComposition)
   .use(createPinia().use(piniaPluginPersistedState))
   .provide(PROVIDE.PB,pb)
